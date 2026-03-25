15ce2fd5132c9888e7e0e32af19a157addb3bc0f
Carlos Zamora <cazamor@microsoft.com>
2026-03-24 19:31:15 -0700

    Add settings for "notifying" on "activity"

--- src/terminal/adapter/adaptDispatch.cpp
+++ src/terminal/adapter/adaptDispatch.cpp
@@ -3640,7 +3640,7 @@ void AdaptDispatch::DoConEmuAction(const std::wstring_view string)
     else if (subParam == 12)
     {
         _pages.ActivePage().Buffer().StartCommand();
-        _api.NotifyShellIntegrationMark();
+        _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::Command);
     }
     else
     {
@@ -3675,7 +3675,7 @@ void AdaptDispatch::DoITerm2Action(const std::wstring_view string)
     if (action == L"SetMark")
     {
         _pages.ActivePage().Buffer().StartPrompt();
-        _api.NotifyShellIntegrationMark();
+        _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::Prompt);
     }
     else
     {
@@ -3713,19 +3713,19 @@ void AdaptDispatch::DoFinalTermAction(const std::wstring_view string)
         case L'A': // FTCS_PROMPT
         {
             _pages.ActivePage().Buffer().StartPrompt();
-            _api.NotifyShellIntegrationMark();
+            _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::Prompt);
             break;
         }
         case L'B': // FTCS_COMMAND_START
         {
             _pages.ActivePage().Buffer().StartCommand();
-            _api.NotifyShellIntegrationMark();
+            _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::Command);
             break;
         }
         case L'C': // FTCS_COMMAND_EXECUTED
         {
             _pages.ActivePage().Buffer().StartOutput();
-            _api.NotifyShellIntegrationMark();
+            _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::Output);
             break;
         }
         case L'D': // FTCS_COMMAND_FINISHED
@@ -3746,7 +3746,7 @@ void AdaptDispatch::DoFinalTermAction(const std::wstring_view string)
             }
 
             _pages.ActivePage().Buffer().EndCurrentCommand(error);
-            _api.NotifyShellIntegrationMark();
+            _api.NotifyShellIntegrationMark(ITerminalApi::ShellIntegrationMark::CommandFinished);
 
             break;
         }
