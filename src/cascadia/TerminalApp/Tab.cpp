07a4f3acfc03d0cbacd96d459ef300d6dcc1940e
Carlos Zamora <cazamor@microsoft.com>
2026-05-04 17:57:55 -0700

    remove .leading from throttledTaskbarProgressChanged

--- src/cascadia/TerminalApp/Tab.cpp
+++ src/cascadia/TerminalApp/Tab.cpp
@@ -1098,7 +1098,6 @@ namespace winrt::TerminalApp::implementation
             dispatcher,
             til::throttled_func_options{
                 .delay = std::chrono::milliseconds{ 200 },
-                .leading = true,
                 .trailing = true,
             },
             [weakThis]() {
